a6089139f9799a863f37b674e8f62da1ab48fbff
DM <dmoosocool@gmail.com>
2022-04-28 15:02:46 +0800

    feat: initial repository

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import '@rss3js/widget'
+import '@rss3js/widget/src/react.d'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <rss3-widget address="0xC8b960D09C0078c18Dcbe7eB9AB9d816BcCa8944"></rss3-widget>
+    </div>
+  )
+}
+
+export default App
